aa93eabe667846aa3624fa2cbd32ebfbd1219003
Hércules S. S. José <hercules_hssj@outlook.com>
2017-02-16 15:01:35 -0200

    Created Helper for simulation

---
+++ GoalService/src/br/uniriotec/aspect/simulation/SimulationHelper.java
@@ -0,0 +1,47 @@
+package br.uniriotec.aspect.simulation;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Helper created for grouping useful methods for simulation
+ * 
+ * @author Hércules S. S. José
+ *
+ */
+public class SimulationHelper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(SimulationHelper.raffleNumbers(10, 60));
+	}
+
+	// static class
+	private SimulationHelper() {}
+	
+	/*
+	 * Generate random number, from 1 to limit
+	 */
+	private static int generateRandomNumber(int limit) {
+		
+		int random = new Random().nextInt(limit);
+		
+		return random;
+	}
+	
+	private static String raffleNumbers(int quantity, int limit) {
+		
+		Set<Integer> raffledNumbers = new TreeSet<>();
+		
+		do {
+			int number = SimulationHelper.generateRandomNumber(limit);
+			
+			raffledNumbers.add(number);
+			
+		} while (raffledNumbers.size() < quantity);
+		
+		return raffledNumbers.toString();
+		
+	}
+}
